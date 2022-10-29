16c335bb530ce69420a12c074a9029dbd4b39c3e
favour-22 <anointed657@gmail.com>
2022-10-29 20:35:57 +0000

    base model

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3 
+"""Defines the base model class"""
+import models
+from datetime import datetime
+from uuid import uuid4
+class BaseModel():
+    """The base model class"""
+    def __init__(self,*agrs,**kwargst)
+
