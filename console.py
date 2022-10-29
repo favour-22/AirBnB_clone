3cafc6154494d52682013eeab53b19bf3751cb91
favour-22 <anointed657@gmail.com>
2022-10-29 21:28:43 +0000

    console

--- console.py
+++ console.py
@@ -5,16 +5,14 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """DEFINES THE HBNB COMMAND INTERPRETER
     Attributes:
-
         prompt (hbnb): The command prompt.
-
     """
     prompt = "(hbnb)"
     
     def empty_line(self):
         """Execute nothing when enter is pressed"""
         pass
-    def do_quit(self ,arg):
+    def do_quit(self, arg):
         """Quit the hbnb prompt when called"""
         return True
     def do_EOF(self, arg):
